9678ae1071285c8bb3f38af76aa5fd6e40d2f0ee
AbhishekTmz <atmz1113@gmail.com>
2023-10-13 08:12:54 +0545

    calc equals button created

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -65,6 +65,7 @@ Row {
 for (i in 7 downTo 1 step 3) CalcRow(display = displayText, startNum = i, numButtons = 3)
                 Row {
                     CalcNumericButton(number = 0, display =displayText )
+                    CalcEqualsButton(display = displayText)
                 }
             }
 
